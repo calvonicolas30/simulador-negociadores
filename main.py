312e3fb40f8a4a84c5ca4864a8d7d8dc3b47df4e
calvonicolas30 <nicolas.calvo@outlook.es>
2026-01-31 15:45:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if "login" not in st.session_state:
 
 if not st.session_state.login:
 
-    st.image("logo_policia.png", width=180)
+    st.image("logo_policia.PNG", width=180)
 
     st.title("Ingreso al Sistema")
 
